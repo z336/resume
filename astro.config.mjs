7a964f7b222c0fa13f724a0eac8aa940af552a4a
Jon Coleman <jcoleman1388@gmail.com>
2025-04-19 20:45:41 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://resume.joncoleman.me/",
+  // Instead of generating a random name based on a file deliberately set the css to /css/styles
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+              return "css/styles-[hash][extname]";
+            }
+            return "_astro/[name]-[hash][extname]";
+          },
+        },
+      },
+    },
+  },
+});
